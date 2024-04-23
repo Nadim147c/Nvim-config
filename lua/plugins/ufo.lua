0d670f6941e708350edd36489fa449847806e241
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2024-04-23 19:55:10 +0600

    feat: add ufo plugin for folding

---
+++ lua/plugins/ufo.lua
@@ -0,0 +1,17 @@
+return {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async", "nvim-treesitter/nvim-treesitter" },
+    event = "BufRead",
+    config = function()
+        require("ufo").setup {
+            provider_selector = function(_, _, _)
+                return { "treesitter", "indent" }
+            end,
+        }
+
+        vim.o.foldcolumn = "1"
+        vim.o.foldlevel = 99
+        vim.o.foldlevelstart = 99
+        vim.o.foldenable = true
+    end,
+}
