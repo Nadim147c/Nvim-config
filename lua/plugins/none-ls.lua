a19f20cbeb5f0eecdacaa1bf8e778f7309963c3c
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2024-04-30 22:59:37 +0600

    feat: update none-ls configuration

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,15 +1,16 @@
 return {
     "nvimtools/none-ls.nvim",
-    opts = function()
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
         local null_ls = require "null-ls"
 
-        local opts = {
+        null_ls.setup {
             sources = {
-                null_ls.builtins.diagnostics.ruff,
+                -- null_ls.builtins.diagnostics.ruff,
                 null_ls.builtins.diagnostics.mypy,
                 null_ls.builtins.diagnostics.eslint_d,
+                null_ls.builtins.code_actions.gitsigns,
             },
         }
-        return opts
     end,
 }
