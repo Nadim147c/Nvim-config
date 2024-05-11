d056c4f7a4126c7a9913e38fba419c726e45fde1
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2024-05-11 13:47:13 +0000

    feat: update tmux navigator replace vim-tmux-navigator to nvim-tmux-nagivator which fixes keybindings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,3 +13,12 @@ vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Put without losing current yank" })
 
 vim.keymap.set("n", "<leader>sr", ":%s/", { desc = "Search and replace for this buffer" })
+
+local nvim_tmux_nav = require "nvim-tmux-navigation"
+
+vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,5 +1,5 @@
 return {
-    "christoomey/vim-tmux-navigator",
+    "alexghergh/nvim-tmux-navigation",
     lazy = false,
     cmd = {
         "TmuxNavigateLeft",
@@ -7,11 +7,5 @@ return {
         "TmuxNavigateUp",
         "TmuxNavigateRight",
         "TmuxNavigatePrevious",
-    },
-    keys = {
-        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
     },
 }
