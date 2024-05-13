34d4bd93b4b167e970eab49a91f68541b067fa2a
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2024-05-13 18:31:23 +0000

    feat: add auto command group

--- lua/custom/vim_enter.lua
+++ lua/custom/vim_enter.lua
@@ -1,5 +1,7 @@
--- Open telescope or nvim-tree on VimEnter
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
+    group = vim.api.nvim_create_augroup("vim_enter_telescope_or_nvim_tree", { clear = true }),
+    pattern = "*",
+    desc = "Highlight selection on yank",
     callback = function(data)
         local directory = vim.fn.isdirectory(data.file) == 1
 
